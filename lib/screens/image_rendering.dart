f8288a2c02c255740df3eb5b0802e39cbe9793b5
chalapathi444 <chalapathinagavarmab@gmail.com>
2021-02-04 10:25:03 +0530

    added image rendering

--- lib/screens/image_rendering.dart
+++ lib/screens/image_rendering.dart
@@ -14,8 +14,51 @@ class ImageRendering extends StatelessWidget {
         centerTitle: true,
         title: Text(
           'Image Rendering',
-          style: TextStyle(color: Colors.pink, letterSpacing: 2, fontSize: 25),
+          style: TextStyle(
+              color: Colors.pink[900], letterSpacing: 2, fontSize: 25),
         ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              "Network image",
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+          ),
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Image.network(
+              "https://www.zappkode.com/images/home/logo.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: Text(
+              "Asset Image",
+              style: TextStyle(color: Colors.black, fontSize: 20),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width * 0.6,
+            child: Image.asset(
+              "lib/images/someimage.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
+        ],
       ),
     );
   }
